0eb6b894e0de37980859e6b76fe56981d9fb6891
mepn4 <mepn4@PEDAGO.LAN>
2018-02-15 16:02:42 +0100

    encore des fichiers à mettre à jour

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	console.log('page d accueil');
 	var title = "Redact'Aide";
-	var css_files = ['style.css', 'color.css', 'home.css']; // rajouter des fichiers css
+	var css_files = ['style.css', 'hoverable.css', 'color.css', 'home.css']; // rajouter des fichiers css
 	var jss_files = ['app.js']; // rajouter des fichiers css
   	res.render('main', { path : 'index', css_files: css_files, jss_files:jss_files, title: title});
   	// res.render('main', { path : 'index', title: title });
 });
 
 module.exports = router;
+
